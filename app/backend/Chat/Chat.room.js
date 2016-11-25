2e9ea6cb5fe5f1a872327f03ac5626ee22dfbb99
jakebillings <jake@jakebillings.com>
2016-11-25 16:56:03 -0600

    Remove js from git

--- app/backend/Chat/Chat.room.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const Room_1 = require("../Room/Room");
-class ChatRoom extends Room_1.Room {
-    constructor(conn, frontend, table) {
-        super(conn, frontend, table);
-    }
-}
-exports.ChatRoom = ChatRoom;
-//# sourceMappingURL=Chat.room.js.map
